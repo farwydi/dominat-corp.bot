0d1fb7ef02911229b856e2ad01ffe93e861ac4c8
Zharikov Leonid <leon.zharikov@gmail.com>
2018-05-04 10:58:36 +0300

    feat: test web hook

--- bot.go
+++ bot.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"gopkg.in/telegram-bot-api.v4"
+	"os"
+	"net/http"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook("https://" + os.Getenv("PROGRAM_NAME") + ".herokuapp.com/" + bot.Token))
+	if err != nil {
+		fmt.Errorf("Problem in setting Webhook", err.Error())
+	}
+
+	updates := bot.ListenForWebhook("/" + bot.Token)
+	go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+
+	for update := range updates {
+		log.Printf("%+v\n", update)
+	}
 }
